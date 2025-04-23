db4fca3c9108734abb35f895ff1d74a369962bcc
FyreKS1 <sergeynosov13@gmail.com>
2025-04-23 11:36:24 +0300

    remove add bin

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 Telegram.WebApp.expand(); // развернуть окно
-
-const user = Telegram.WebApp.initDataUnsafe;
-
-Telegram.WebApp.MainButton.text = "Добавить в корзину";
-Telegram.WebApp.MainButton.show();
